f61931c1392623ed07176690abd95dfc1453cdaf
chenwenbin03 <chenwenbin03@meituan.com>
2019-03-25 19:26:03 +0800

    少了一个包

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import router from './router';
 import store, { history } from './redux/configureStore';
 
@@ -19,4 +19,4 @@ ReactDOM.render(React.createElement(getRootComponent(router)), document.getEleme
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
